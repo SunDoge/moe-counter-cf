e1edf0a9703b7a3bc192c73987865eff1499d38b
SunDoge <384813529@qq.com>
2023-11-14 15:53:14 +0800

    finish

--- src/index.ts
+++ src/index.ts
@@ -8,35 +8,18 @@ type Bindings = {
     DB: D1Database;
 }
 
-
 const app = new Hono<{ Bindings: Bindings }>()
 
-// app.use('/*', cors())
+app.use('/*', cors())
 app.get('/', (c) => c.text('Hello Hono!'))
-app.get('/api/:name', async (c) => {
-    const name = c.req.param("name")
-    const add = c.req.query('add')
-    console.log(name)
-    const counter = await getNum(c.env.DB, name);
-
-    if (add !== '0') {
-        c.executionCtx.waitUntil(addNum(c.env.DB, name))
-    }
-
-    // await setNum(c.env.DB, name, 10)
-    return c.text(`name: ${name}, num: ${counter.num}`)
-});
-
-
 app.get('/:name', async (c) => {
     const name = c.req.param('name');
     const theme = c.req.query('theme') || 'gelbooru'
-    const length = c.req.query('length') || 'auto'
+    const length = c.req.query('length') || '7'
     const add = c.req.query('add') !== '0'
     const pixelated = c.req.query('pixelated') === 'pixelated'
 
     const db = c.env.DB;
-
     const counter = await getNum(db, name);
 
     let image;
@@ -46,30 +29,9 @@ app.get('/:name', async (c) => {
     } else {
         image = generateImage(counter.num, theme, length, pixelated);
     }
-    console.log(image)
     return c.body(image, 200, {
         'Content-Type': 'image/svg+xml; charset=utf-8',
     })
 });
-
-// app.get("/:id", async (c) => {
-//     const id = c.req.param("id");
-//     const add = parseInt(c.req.query('add') || '1')
-
-//     const count = Number.parseInt(await c.env.KV.get(id) || "0");
-
-//     if (add !== 0) {
-//         c.executionCtx.waitUntil(
-//             c.env.KV.put(id, (count + 1).toString())
-//         )
-//     }
-
-//     // c.header("Content-Type", 'image/svg+xml; charset=utf-8');
-//     return c.body("asd", 200, {
-//         'Content-Type': 'image/svg+xml; charset=utf-8',
-//     })
-// })
-
-
 
 export default app
